ff77f843210f6cc0dc2a5c541a22a6650ed600b8
nahian-islam <nahianku1@gmail.com>
2022-02-19 09:07:39 +0600

    2nd commit

--- shemas/todoshema.js
+++ shemas/todoshema.js
@@ -3,17 +3,20 @@ let mongoose = require('mongoose');
 let todoshema=mongoose.Schema({
     title:{
         type:String,
+        required:true,
     },
     description:{
         type:String,
+        required:true,
     },
     status:{
         type:String,
         enum:['done','pending'],
+        required:true,
     },
     date:{
         type:Date,
-        default:new Date().toUTCString(),
+        default:new Date().getDate()
     }
 })
 module.exports =todoshema
