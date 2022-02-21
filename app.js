e3d181355d804831afb159a3eedd5471242a4f29
nahian-islam <nahianku1@gmail.com>
2022-02-21 18:04:42 +0600

    1st commit

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 let express = require('express');
 let mongoose = require('mongoose');
 let todorouter = require('./todos/todorouter');
-let app=express();
-let PORT=process.env.PORT || 3000;
+let app = express();
+let PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost/todos',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+
+const uri = `mongodb+srv://nahianku1:bashadmin123@mongo-cluster-1.1b5ei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(()=>console.log('Connection Successful!'))
-.catch(err=>console.log(err));
+    .then(() => console.log('Connection Successful!'))
+    .catch(err => console.log(err));
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./views/temp/'));
-app.use('/todo',todorouter);
+app.use('/todo', todorouter);
 
-app.listen(PORT,()=>{
-    console.log("Server listening on http://localhost:"+PORT);
+app.listen(PORT, () => {
+    console.log("Server listening on http://localhost:" + PORT);
 })
