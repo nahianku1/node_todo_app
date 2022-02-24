228996a4807754586410df5be91722b1968e8c67
nahian-islam <nahianku1@gmail.com>
2022-02-24 20:11:02 +0600

    ajax live name

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let mongoose = require('mongoose');
 let todorouter = require('./todos/todorouter');
+const {response} = require("express");
 let app = express();
 let PORT = process.env.PORT || 3000;
+
+
+
 
 
 const uri = process.env.MONGODB_URI || 'mongodb://localhost/todos';
@@ -14,7 +18,7 @@ mongoose.connect(uri, {
     .then(() => console.log('Connection Successful!'))
     .catch(err => console.log(err));
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('./views/temp/'));
 app.use('/todo', todorouter);
 
--- todos/todorouter.js
+++ todos/todorouter.js
@@ -4,11 +4,16 @@ let todoRouter = express.Router();
 let todoshema = require('../shemas/todoshema');
 let todomodel = new mongoose.model('todo', todoshema);
 
+let sid = "AC3f59d6e91d38058390769d81c7365bad";
+let auth_token = "dd63b338d49101b8d4f1f530e2245f27";
+
+let twilio = require('twilio')(sid, auth_token);
+
 
 todoRouter.get('/', async (req, res) => {
     await todomodel.find().exec((err, data) => {
         if (err) {
-            res.status(500).json({ message: "There was an Error" })
+            res.status(500).json({message: "There was an Error"})
         } else {
             res.render('temp/index')
         }
@@ -17,26 +22,26 @@ todoRouter.get('/', async (req, res) => {
 })
 
 todoRouter.get('/table', async (req, res) => {
-    await todomodel.find().sort({ date: -1 }).exec((err, data) => {
+    await todomodel.find().sort({date: -1}).exec((err, data) => {
         if (err) {
-            res.status(500).json({ message: "There was an Error" })
+            res.status(500).json({message: "There was an Error"})
         } else {
 
-            res.json({ data })
+            res.json({data})
         }
     })
 
 })
 
 todoRouter.post('/checkname', async (req, res) => {
-    // console.log(req.body);
-    await todomodel.find({ title: req.body.title }).exec((err, data) => {
+    console.log(req.body);
+    await todomodel.find({title: req.body.title}).exec((err, data) => {
         if (err) {
             res.sendStatus(500)
         } else {
-            if(data.length===0) {
-                res.json({success: true})
-            }else{
+            if (data.length === 0) {
+                res.sendStatus(200)
+            } else {
                 res.sendStatus(500)
             }
         }
@@ -56,6 +61,39 @@ todoRouter.post('/', async (req, res) => {
             res.sendStatus(500)
         } else {
             res.sendStatus(200)
+
+            let smsid;
+
+            async function getID() {
+                await todomodel.find().limit(1).sort({date: -1}).exec((err, data) => {
+
+                    if (err) {
+                        console.log("FIND ERROR")
+                    } else {
+                        data.forEach((value) => {
+                            smsid = value._id
+                        })
+
+                        twilio.messages.create({
+                            from: "+19034379652",
+                            to: "+8801779137132",
+                            body: `Task Created Successfully! Your task ID is ${smsid}`
+                        })
+                            .then((res) => {
+                                console.log("Message has sent!")
+                            })
+                            .catch((e) => {
+                                console.log(e)
+                            })
+                    }
+                })
+
+
+            }
+
+            getID();
+
+
         }
     })
 })
@@ -65,7 +103,7 @@ todoRouter.put('/', async (req, res) => {
         req.body.date = req.body.presentdate
     }
 
-    await todomodel.updateOne({ _id: req.body._id }, {
+    await todomodel.updateOne({_id: req.body._id}, {
         $set: {
             title: req.body.title,
             description: req.body.description,
@@ -84,7 +122,7 @@ todoRouter.put('/', async (req, res) => {
 
 todoRouter.delete('/', async (req, res) => {
 
-    await todomodel.deleteMany({ _id: req.body._id }, (err) => {
+    await todomodel.deleteMany({_id: req.body._id}, (err) => {
         if (err) {
 
             res.send('0')
